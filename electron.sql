5c1c65d4aada19af4b60bc687cb3392aad3369b7
LiggPlus <wenzhouli06@gmail.com>
2025-05-15 19:07:57 +0800

    更新qsl

--- electron.sql
+++ electron.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80040 (8.0.40)
  File Encoding         : 65001
 
- Date: 29/04/2025 09:59:17
+ Date: 15/05/2025 19:07:43
 */
 
 SET NAMES utf8mb4;
@@ -22,16 +22,46 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `admin_users`;
 CREATE TABLE `admin_users`  (
-  `user_id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL,
   `account` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL COMMENT '账号',
   `nick_name` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NULL DEFAULT NULL COMMENT '昵称',
-  `password` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL COMMENT '密码',
+  `password` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL COMMENT '密码',
   `email` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NULL DEFAULT NULL COMMENT '邮箱',
   `created_at` datetime NULL DEFAULT NULL COMMENT '创建时间',
   `updated_at` datetime NULL DEFAULT NULL COMMENT '更新时间',
   `user_avatar` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NULL DEFAULT NULL,
+  `login_time` datetime NULL DEFAULT NULL COMMENT '登录时间',
+  `phone_number` bigint UNSIGNED NULL DEFAULT NULL COMMENT '号码',
   INDEX `admin_users_user_id_index`(`user_id` ASC) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 8259543157 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_bin ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for admin_web_user
+-- ----------------------------
+DROP TABLE IF EXISTS `admin_web_user`;
+CREATE TABLE `admin_web_user`  (
+  `admin_id` bigint NOT NULL AUTO_INCREMENT,
+  `account` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_ci NOT NULL,
+  `nick_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_ci NULL DEFAULT NULL,
+  `email` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_ci NULL DEFAULT NULL,
+  `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_ci NULL DEFAULT NULL,
+  `phone_number` bigint NULL DEFAULT NULL COMMENT '手机号码',
+  `login_time` datetime NULL DEFAULT NULL COMMENT '最后登录时间',
+  `login_ip` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_ci NULL DEFAULT NULL COMMENT '登录ip',
+  PRIMARY KEY (`admin_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 11825954457 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_as_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for announcement
+-- ----------------------------
+DROP TABLE IF EXISTS `announcement`;
+CREATE TABLE `announcement`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_ci NOT NULL,
+  `create_time` datetime NOT NULL COMMENT '发布时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `announcement_id_index`(`id` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_as_ci COMMENT = '公告' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for phone
@@ -47,16 +77,27 @@ CREATE TABLE `phone`  (
   `phone_region_id` int NULL DEFAULT NULL COMMENT '号码归属地区',
   PRIMARY KEY (`phone_id`) USING BTREE,
   UNIQUE INDEX `idx_phone_number`(`phone_number` ASC) USING BTREE COMMENT '手机号唯一索引'
-) ENGINE = InnoDB AUTO_INCREMENT = 72 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_tr_0900_as_cs ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 129 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_tr_0900_as_cs ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for phone_project_relation
+-- ----------------------------
+DROP TABLE IF EXISTS `phone_project_relation`;
+CREATE TABLE `phone_project_relation`  (
+  `id` bigint NOT NULL,
+  `project_id` bigint NOT NULL,
+  `phone_id` bigint NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_as_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for project
 -- ----------------------------
 DROP TABLE IF EXISTS `project`;
 CREATE TABLE `project`  (
-  `project_id` int NOT NULL AUTO_INCREMENT,
+  `project_id` bigint NOT NULL,
   `project_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `project_price` decimal(10, 2) NOT NULL COMMENT '价格',
+  `project_price` float NULL DEFAULT NULL COMMENT '价格',
   `project_created_at` datetime NOT NULL COMMENT '项目创建时间',
   PRIMARY KEY (`project_id`) USING BTREE,
   INDEX `project_project_id_index`(`project_id` ASC) USING BTREE
@@ -99,22 +140,23 @@ CREATE TABLE `user_order`  (
   `project_id` int NOT NULL,
   PRIMARY KEY (`user_project_id`) USING BTREE,
   INDEX `user_order_user_project_id_index`(`user_project_id` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 14 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户购买的订单' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 216 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户购买的订单' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for users
 -- ----------------------------
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users`  (
-  `user_id` bigint NOT NULL AUTO_INCREMENT,
-  `account` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_ci NULL DEFAULT NULL,
-  `password` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_ci NULL DEFAULT NULL,
+  `user_id` bigint NOT NULL,
+  `account` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_ci NOT NULL,
+  `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_ci NOT NULL,
   `nick_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_ci NULL DEFAULT NULL,
   `email` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_ci NULL DEFAULT NULL,
   `created_at` datetime NULL DEFAULT NULL,
   `updated_at` datetime NULL DEFAULT NULL,
   `user_avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_ci NULL DEFAULT NULL,
   `user_status` int NOT NULL DEFAULT 1 COMMENT '用户状态(1=‘正常’   0=‘已注销’)',
+  `login_time` datetime NULL DEFAULT NULL COMMENT '的登陆时间',
   PRIMARY KEY (`user_id`) USING BTREE,
   INDEX `users_user_id_index`(`user_id` ASC) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 8259543160 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_as_ci ROW_FORMAT = Dynamic;
