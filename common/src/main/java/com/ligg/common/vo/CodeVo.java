2f87cef92896842150f28da40a0b3c8798ec86d1
LiggPlus <wenzhouli06@gmail.com>
2025-05-24 09:19:32 +0800

    获取验证码增加id、createTime字段

--- common/src/main/java/com/ligg/common/vo/CodeVo.java
+++ common/src/main/java/com/ligg/common/vo/CodeVo.java
@@ -2,9 +2,12 @@ package com.ligg.common.vo;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class CodeVo {
-
+    private Integer id;
     private String code;
     private Long phoneNumber;
+    private LocalDateTime createTime;
 }
